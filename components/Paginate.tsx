27b02049bd958926741c6a10f9a296f07c97f8e4
Mohamed Achaq <motriyanox@gmail.com>
2022-05-10 21:42:24 +0100

    fixing pagination returns

--- components/Paginate.tsx
+++ components/Paginate.tsx
@@ -10,7 +10,12 @@ const Paginate = (
 ) => {
   const startIndex = (currentPage - 1) * pageSize
 
+  if(items.length === 0) {
+    return []
+  }
+
   return _(items).slice(startIndex).take(pageSize).value()
+  
 }
 
 export default Paginate
