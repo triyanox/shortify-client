f44495c7711d133a7a5793ee081110021cbd8474
Mohamed Achaq <motriyanox@gmail.com>
2022-05-22 01:48:10 +0100

    Fixing the mapping issue

--- components/Paginate.tsx
+++ components/Paginate.tsx
@@ -1,21 +1,22 @@
 import _ from 'lodash'
 
 const Paginate = (
-  items: {
-    original_url: string
-    short_url: string
-  }[],
+  items:
+    | {
+        original_url: string
+        short_url: string
+      }[]
+    | null,
   pageSize: number,
   currentPage: number
 ) => {
   const startIndex = (currentPage - 1) * pageSize
 
-  if(items.length === 0) {
+  if (items && items.length === 0) {
     return []
   }
 
   return _(items).slice(startIndex).take(pageSize).value()
-  
 }
 
 export default Paginate
