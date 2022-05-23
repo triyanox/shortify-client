aa2efebe1dc4d8be79efcd67a8ee09ac50e68146
Mohamed Achaq <motriyanox@gmail.com>
2022-05-24 00:32:23 +0100

    indicating loading state

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,26 @@
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <section className="mt-28 flex w-full items-center  justify-center py-8  px-10 md:px-24 lg:mt-4  xl:px-20">
-      <h1 className="text-lg   md:text-xl">
-        Shortify made by{' '}
-        <Link href="https://achaq.codes" passHref>
+    <footer className="dark:text-bright-gray-50 my-8 mt-24 px-8 text-zinc-800 dark:text-zinc-200 md:px-24 xl:px-8">
+      <div className="container mx-auto flex flex-col items-center justify-center px-5 py-8 sm:flex-row">
+        <a className="title-font flex items-center justify-center font-medium text-zinc-800 dark:text-zinc-200 lg:justify-start">
+          <span className="ml-3 hidden text-xl text-[#FFBA49] dark:text-[#EF5B5B] md:block">
+            Shortify
+          </span>
+        </a>
+        <p className="text-md md:text-auto mt-4 text-center text-zinc-800 dark:text-zinc-200 sm:ml-4 sm:mt-0 sm:border-l-2 sm:py-2 sm:pl-4">
+          © 2022 Shortify — Made By
           <a
-            target={'_blank'}
-            className="cursor-pointer text-[#FFBA49] dark:text-[#EF5B5B]"
+            href="https://achaq.codes"
+            className="ml-2 text-xl  text-[#FFBA49] dark:text-[#EF5B5B]"
+            rel="noopener noreferrer"
+            target="_blank"
           >
-            Mohamed Achaq
+            Achaq
           </a>
-        </Link>
-      </h1>
-    </section>
+        </p>
+      </div>
+    </footer>
   )
 }
 
