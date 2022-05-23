b0f63fb0e467eab3aee3f47f67bd11e13b706118
Mohamed Achaq <motriyanox@gmail.com>
2022-05-24 00:32:35 +0100

    indicating loading state

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -36,14 +36,14 @@ const SignUp = () => {
     }
     const register = userService.registerUser(data)
     try {
-       toast.promise(register, {
+      toast.promise(register, {
         loading: 'Loading',
         success: 'Successfully registered , redirecting...',
         error: 'Unable to register',
       })
       const registered = await register
       localStorage.setItem('jwt', registered.headers['x-auth-token'])
-     
+
       setTimeout(() => {
         window.location.replace('/')
       }, 2000)
@@ -55,7 +55,7 @@ const SignUp = () => {
   }
 
   return (
-    <section className="mt-28 flex w-full flex-col-reverse items-center justify-center gap-y-8 px-10 md:px-24 lg:mt-0 lg:h-screen lg:flex-row xl:px-20">
+    <section className="mt-8 flex h-screen w-full flex-col-reverse items-center justify-center gap-y-8 px-10 md:px-24 lg:mt-0 lg:h-screen lg:flex-row xl:px-20">
       <div className="mx-8 flex w-full flex-col items-center justify-center gap-4 sm:mx-16 lg:mx-0">
         <h1 className="mb-8 animate-gradient-x bg-gradient-to-r from-[#FFBA49] to-[#EF5B5B] bg-clip-text text-4xl text-transparent">
           Sign up
@@ -64,7 +64,7 @@ const SignUp = () => {
           className="flex w-full flex-col items-center justify-center gap-2"
           onSubmit={handleSubmit}
         >
-          <div className="mb-4  w-5/6 md:w-[24rem] lg:w-[28rem]">
+          <div className="mb-4  w-11/12 md:w-[24rem] lg:w-[28rem]">
             <input
               className="w-full appearance-none rounded-lg bg-gray-200  py-3 px-4 leading-tight text-gray-900  outline-none transition-all duration-500 focus:outline focus:outline-[#FFBA49] dark:bg-zinc-800 dark:text-gray-200 dark:focus:outline dark:focus:outline-[#EF5B5B]"
               id="username"
@@ -75,7 +75,7 @@ const SignUp = () => {
               required
             />
           </div>
-          <div className="mb-4 w-5/6 md:w-[24rem] lg:w-[28rem]">
+          <div className="mb-4 w-11/12 md:w-[24rem] lg:w-[28rem]">
             <input
               className="w-full appearance-none rounded-lg bg-gray-200 py-3 px-4 leading-tight text-gray-900  outline-none transition-all duration-500 focus:outline focus:outline-[#FFBA49] dark:bg-zinc-800 dark:text-gray-200 dark:focus:outline dark:focus:outline-[#EF5B5B]"
               id="email"
@@ -86,7 +86,7 @@ const SignUp = () => {
               required
             />
           </div>
-          <div className=" w-5/6 md:w-[24rem] lg:w-[28rem]">
+          <div className=" w-11/12 md:w-[24rem] lg:w-[28rem]">
             <input
               className="relative w-full appearance-none rounded-lg bg-gray-200  py-3 px-4 leading-tight text-gray-900  outline-none transition-all duration-500 focus:outline focus:outline-[#FFBA49] dark:bg-zinc-800 dark:text-gray-200 dark:focus:outline dark:focus:outline-[#EF5B5B]"
               id="password"
@@ -107,7 +107,7 @@ const SignUp = () => {
             </button>
           </div>
 
-          <div className="flex w-5/6 flex-col items-center justify-center gap-4 md:w-[24rem] lg:w-[28rem]">
+          <div className="flex w-11/12 flex-col items-center justify-center gap-4 md:w-[24rem] lg:w-[28rem]">
             <button
               className="text-md  mt-8 w-full rounded-lg bg-[#FFBA49] p-4 py-2 text-white transition-all duration-200 ease-in hover:scale-105 active:scale-90  dark:bg-[#EF5B5B] dark:text-black md:text-lg "
               type="submit"
