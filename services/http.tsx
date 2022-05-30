d91b106d31de36c2e15a2a3fde3f3d753a335987
Mohamed Achaq <motriyanox@gmail.com>
2022-05-30 21:36:31 +0100

    return the response

--- services/http.tsx
+++ services/http.tsx
@@ -4,18 +4,18 @@ import * as auth from './authService'
 if (typeof window !== 'undefined') {
   axios.defaults.headers.common['x-auth-token'] = auth.getJwt()
 }
-axios.interceptors.response.use(undefined, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500
-  if (!expectedError) {
-    if (typeof window !== 'undefined') {
-      window.location.href = '/error'
+axios.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem('jwt')
+      window.location.href = '/login'
     }
+    return Promise.reject(error)
   }
-  return Promise.reject(error)
-})
+)
 
 export default {
   get: axios.get,
